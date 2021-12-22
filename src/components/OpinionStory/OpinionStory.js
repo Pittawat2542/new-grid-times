6d4fc050248f70b1af7fcb345265699bd1536f41
Pittawat <pittawat2542@gmail.com>
2021-12-22 16:53:18 +0700

    feat: opnion story avatar

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,3 +1,4 @@
+import { QUERIES } from '../../constants';
 import React from 'react';
 import styled from 'styled-components/macro';
 
@@ -17,6 +18,13 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: column;
+  }
 `;
 
 const Avatar = styled.img`
@@ -25,6 +33,11 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  margin-left: 16px;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-left: revert;
+  }
 `;
 
 const AuthorName = styled.p`
