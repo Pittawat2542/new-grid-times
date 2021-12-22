7153fa1114d28681f320f049818919a2775fa12f
Pittawat <pittawat2542@gmail.com>
2021-12-22 16:45:07 +0700

    feat: story divider

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -1,79 +1,96 @@
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from '../../data';
+
+import Advertisement from '../Advertisement';
+import MainStory from '../MainStory';
+import OpinionStory from '../OpinionStory';
 import React from 'react';
+import SecondaryStory from '../SecondaryStory';
+import SectionTitle from '../SectionTitle';
 import styled from 'styled-components/macro';
 
-import {
-  MAIN_STORY,
-  OPINION_STORIES,
-  SECONDARY_STORIES,
-} from '../../data';
-
-import SectionTitle from '../SectionTitle';
-import MainStory from '../MainStory';
-import SecondaryStory from '../SecondaryStory';
-import OpinionStory from '../OpinionStory';
-import Advertisement from '../Advertisement';
-
 const MainStoryGrid = () => {
-  return (
-    <Wrapper>
-      <MainStorySection>
-        <MainStory {...MAIN_STORY} />
-      </MainStorySection>
+	return (
+		<Wrapper>
+			<MainStorySection>
+				<MainStory {...MAIN_STORY} />
+			</MainStorySection>
 
-      <SecondaryStorySection>
-        <StoryList>
-          {SECONDARY_STORIES.map((story, index) => (
-            <SecondaryStory key={story.id} {...story} />
-          ))}
-        </StoryList>
-      </SecondaryStorySection>
+			<SecondaryStorySection>
+				<StoryList>
+					{SECONDARY_STORIES.map((story, index) =>
+						index < SECONDARY_STORIES.length - 1 ? (
+							<>
+								<SecondaryStory key={story.id} {...story} />
+								<Divider />
+							</>
+						) : (
+							<SecondaryStory key={story.id} {...story} />
+						)
+					)}
+				</StoryList>
+			</SecondaryStorySection>
 
-      <OpinionSection>
-        <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
-          {OPINION_STORIES.map((story, index) => (
-            <OpinionStory key={story.id} {...story} />
-          ))}
-        </StoryList>
-      </OpinionSection>
+			<OpinionSection>
+				<SectionTitle>Opinion</SectionTitle>
+				<StoryList>
+					{OPINION_STORIES.map((story, index) =>
+						index < OPINION_STORIES.length - 1 ? (
+							<>
+								<OpinionStory key={story.id} {...story} />
+								<Divider />
+							</>
+						) : (
+							<OpinionStory key={story.id} {...story} />
+						)
+					)}
+				</StoryList>
+			</OpinionSection>
 
-      <AdvertisementSection>
-        <Advertisement />
-      </AdvertisementSection>
-    </Wrapper>
-  );
+			<AdvertisementSection>
+				<Advertisement />
+			</AdvertisementSection>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-areas:
-    'main-story'
-    'secondary-stories'
-    'opinion-stories'
-    'advertisement';
-  gap: 48px;
-  margin-bottom: 48px;
+	display: grid;
+	grid-template-areas:
+		'main-story'
+		'secondary-stories'
+		'opinion-stories'
+		'advertisement';
+	gap: 48px;
+	margin-bottom: 48px;
 `;
 
 const MainStorySection = styled.section`
-  grid-area: main-story;
+	grid-area: main-story;
 `;
 
 const SecondaryStorySection = styled.section`
-  grid-area: secondary-stories;
+	grid-area: secondary-stories;
 `;
 
 const StoryList = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 `;
 
 const OpinionSection = styled.section`
-  grid-area: opinion-stories;
+	grid-area: opinion-stories;
 `;
 
 const AdvertisementSection = styled.section`
-  grid-area: advertisement;
+	grid-area: advertisement;
+`;
+
+const Divider = styled.hr`
+	height: 1px;
+	color: var(--color-gray-300);
+	width: 100%;
+	margin-top: 16px;
+	margin-bottom: 16px;
 `;
 
 export default MainStoryGrid;
